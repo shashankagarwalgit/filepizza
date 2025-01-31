4fead9e8cd912e2b2afdfc51e87dcfeb7d7677c7
Shashank Agarwal <162678882+shashankagarwalgit@users.noreply.github.com>
2025-01-31 22:59:06 +0530

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable strict mode to avoid calling useEffect twice in development.
-  // The uploader and downloader are both using useEffect to listen for peerjs events
-  // which causes the connection to be created twice.
-  reactStrictMode: false,
-  output: 'standalone'
-}
+  output: 'export',
+  distDir: 'dist',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
