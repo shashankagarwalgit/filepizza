5d1b3287a25bad357454e76efe077b2686aae58d
Shashank Agarwal <162678882+shashankagarwalgit@users.noreply.github.com>
2025-01-31 22:44:35 +0530

    removed some files

--- scripts/pull-and-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-git pull origin main
-sudo docker pull kern/filepizza:latest
-sudo docker compose -f docker-compose.production.yml up -d
-sudo docker compose logs -f
